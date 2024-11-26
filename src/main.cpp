818b27cec695c881c0883ee22b9ee994dc926670
QuantaNet <quantanet@QuantaNets-MacBook-Pro.local>
2024-11-26 09:10:02 -0500

    removed unecessary code for shader file paths;

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1baae0dba723335c6dab0f5055aa00678c699e2daaf61f4f510a5d128486eb1b
-size 32554
+oid sha256:2281d5fd427f6e36e1f4c91907288504673fe81d477a33da3aeaea8551de9408
+size 32391
